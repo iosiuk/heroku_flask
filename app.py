221272d1f3eca68bfad87dcda7f8477b12acab2f
Dmitry Osiyuk <dmitry.osiyuk@gmail.com>
2019-10-31 17:14:43 +0200

    edit app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 # app.py
 from flask import Flask, request, jsonify, render_template
-import requests
 app = Flask(__name__)
 
 @app.route('/getmsg/', methods=['GET'])
@@ -44,15 +43,7 @@ def post_something():
 
 @app.route('/check_ga/', methods=['GET'])
 def check_ga():
-	url = request.args.get("url", None)
-    try:
-        headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) '
-                                 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
-        response = requests.get(url, headers=headers)
-    except:
-        response.status_code = 'Checking error.'
-    return 'For URL: {} response code are: {}'.format(url, response.status_code)
-    
+    return "OK"
 
 # A welcome message to test our server
 @app.route('/')
