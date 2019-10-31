5fb30f22cf477bf89f5f88c80d5211a400f2b965
Dmitry Osiyuk <dmitry.osiyuk@gmail.com>
2019-10-31 17:08:48 +0200

    edit app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # app.py
 from flask import Flask, request, jsonify, render_template
+import requests
 app = Flask(__name__)
 
 @app.route('/getmsg/', methods=['GET'])
@@ -43,12 +44,20 @@ def post_something():
 
 @app.route('/check_ga/', methods=['GET'])
 def check_ga():
-    return "OK"
+	url = request.args.get("url", None)
+    try:
+        headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) '
+                                 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
+        response = requests.get(url, headers=headers)
+    except:
+        response.status_code = 'Checking error.'
+    return 'For URL: {} response code are: {}'.format(url, response.status_code)
+    
 
 # A welcome message to test our server
 @app.route('/')
 def index():
-    return render_template('index.html') #render_template('index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
