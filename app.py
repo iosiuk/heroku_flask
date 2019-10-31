4a874a308bee979fc31389d3cc7819848066ea96
Dmitry Osiyuk <dmitry.osiyuk@gmail.com>
2019-10-31 16:43:39 +0200

    edit app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def check_ga():
 # A welcome message to test our server
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "<h1>Всем привет!</h1>"#render_template('index.html')
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
