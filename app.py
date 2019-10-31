9ca5a0b7f4a02b34e4a90f06baf68af743646e4c
Dmitry Osiyuk <dmitry.osiyuk@gmail.com>
2019-10-31 17:31:01 +0200

    edit app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def check_ga():
 		response = requests.get(url, headers=headers)
 	except:
 		response.status_code = 'Checking error.'
-	return response.status_code
+	return url
  
  #    return 'For URL: {} response code are: {}'.format(url, response.status_code)
 
